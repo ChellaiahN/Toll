6401549a928aabcd72f4bb25a37c4230b6a4e47d
ChellaiahN <101564247+ChellaiahN@users.noreply.github.com>
2022-09-22 23:14:46 +0530

    Update AddToll.js

--- src/AddToll.js
+++ src/AddToll.js
@@ -92,7 +92,7 @@ function AddToll(){
                     <input onChange={(a)=>{setReturn3(a.target.value)}} value={returnn3} type="number" placeholder="Return Journey" ></input><br></br>
                     
                     
-                    <Link to="/viewtolls"><button onClick={()=>{savetoll();handleToClose()}}>submit</button></Link><span> </span>
+                    <Link to="/viewtolls"><button onClick={savetoll}>submit</button></Link><span> </span>
                     <Link to="/viewtolls"><button onClick={handleToClose}>cancel</button></Link>
                 </form>
             </dialog>
